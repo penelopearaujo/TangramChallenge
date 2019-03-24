2978960e1677a10f32f81ccf778a99b48bb81866
Penélope Araújo <pmpa@cin.ufpe.br>
2019-03-24 20:26:42 -0300

    The Lord has helped us so far.

--- TangramChallenge-PenelopeAraujo.playground/Contents.swift
+++ TangramChallenge-PenelopeAraujo.playground/Contents.swift
@@ -1,3 +1,22 @@
+/*:
+ # Tangram Challenge
+ ![Playground Icon](tangram.png width="200" height="200")
+ 
+ ### About
+ 
+ Tangram Challenge is a puzzle game for teaching children logical thinking basic geometric shapes and logical thinking. This game was made using SpriteKit and UIKit frameworks.
+
+ ### Gameplay
+ 
+ First, the child has to associate the names of the geometric shapes to the corresponding shape. Then, the child has to use the shapes to solve a challenge and create a rocket!
+ 
+ ### Requirements
+ 
+ To play the game, you have to build and interact with the Playground in the latest release of XCode (10.1).
+ 
+ ---
+ */
+
 import PlaygroundSupport
 import SpriteKit
 import UIKit
